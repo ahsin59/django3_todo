16ce3b7edc56bba4478eac42d5a3d3be82bffbab
Ubuntu <ubuntu@ip-172-31-37-52.ec2.internal>
2019-10-02 03:04:43 +0000

    implement enviroment specific values

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,10 +27,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', 's&82ekhy7kvp&d1o4ahggl(#3g$ly70*uaoaul^mu_tw(rj)p!')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
-ALLOWED_HOSTS = [os.environ.get('cloud9_HOSTNAME').
-                os.environ.get('HOSTNAME') ]
+ALLOWED_HOSTS = [os.environ.get('AWS_Cloud9_HOSTNAME').
+                 os.environ.get('HOSTNAME')]
 
 
 # Application definition
@@ -79,15 +79,15 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#       'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#    }
-# }
-
-DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if development:
+      DATABASES = {
+         'default': {
+             'ENGINE': 'django.db.backends.sqlite3',
+              'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+  }
+else:
+     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
